1ddfa6b010b8cbb5dd1e94c02c0ef8c973ce072a
Shannonbyte <shannondaly0408@gmail.com>
2022-03-06 15:16:51 -0700

    connected client to api (hopefully)

--- q-a-app/src/Home-Page.js
+++ q-a-app/src/Home-Page.js
@@ -5,11 +5,27 @@ import Body from './Body.js';
 
 class HomePage extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: '' };
+}
+
+callAPI() {
+    fetch('http://localhost:9000/test')
+        .then(res => res.text())
+        .then(res => this.setState({ apiResponse: res }));
+}
+
+componentWillMount() {
+    this.callAPI();
+}
+
   render() {
     return (
       <div className = 'homepage'>
         <Header/>
         <Body/>
+        <p className='api-response'>;{this.state.apiResponse}</p>
         <Footer/>
       </div>
     )
