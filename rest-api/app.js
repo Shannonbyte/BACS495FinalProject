840d37830af4139f720046c578d8d9aaf3c2e2b3
Shannonbyte <shannondaly0408@gmail.com>
2022-03-06 15:07:37 -0700

    added test js and route to app.js

--- rest-api/app.js
+++ rest-api/app.js
@@ -5,6 +5,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var testAPIRouter = require("./routes/test");
+
 
 var app = express();
 
@@ -16,5 +18,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use("/test", testAPIRouter);
 
 module.exports = app;
