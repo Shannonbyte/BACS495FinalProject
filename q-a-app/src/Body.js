5d192fb68c4ae703de6bf6c81ca0b6d54500dfb0
Shannonbyte <shannondaly0408@gmail.com>
2022-03-06 14:32:56 -0700

    added bootstrap and body component

---
+++ q-a-app/src/Body.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './Body.css'
+
+class Body extends Component{
+  render(){
+    return (
+      <div className="container-justify-content-start">
+        <div className="row">
+          <div className="col">
+            <h2>Sidebar</h2>
+          </div>
+          <div class="col-7">
+            <h2>Main Content (placeholder for posts)</h2>
+          </div>
+          <div class="col">
+            <h2>Right Sidebar</h2>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Body;
--- q-a-app/src/Home-Page.js
+++ q-a-app/src/Home-Page.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import Header from './Header.js';
 import Footer from './Footer.js';
+import Body from './Body.js';
 
 class HomePage extends Component {
+
   render() {
     return (
       <div className = 'homepage'>
         <Header/>
+        <Body/>
         <Footer/>
       </div>
     )
